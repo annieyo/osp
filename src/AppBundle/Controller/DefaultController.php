4c6d78b7aed9c2d4b8433642b5400e776057e64e
david <david.bigus@gmail.com>
2016-09-05 09:08:58 +0200

    - Login added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 
 class DefaultController extends Controller
 {
@@ -13,9 +16,48 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+//        $user = $this->getUser();
+//        if (!($user instanceof UserInterface)) {
+//            return $this->redirectToRoute('login');
+//        } else {
+
+//            $user = $this->get('security.token_storage')->getToken()->getUser();
+//
+//            var_dump($user);
+//            exit;
+
+            return $this->render('default/index.html.twig', [
+                'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            ]);
+//        }
+    }
+
+
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $user = $this->getUser();
+        if ($user instanceof UserInterface) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        /** @var AuthenticationException $exception */
+        $exception = $this->get('security.authentication_utils')
+            ->getLastAuthenticationError();
+
+        return $this->render('default/login.html.twig', [
+            'error' => $exception ? $exception->getMessage() : NULL,
         ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction(Request $request)
+    {
+        // Nothing needed.
     }
 }
