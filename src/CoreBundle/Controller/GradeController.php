7717d79159e827c69c9ba277ca2c4fca0fa72e9c
david <david.bigus@gmail.com>
2016-09-08 00:13:41 +0200

    - excel import functions

--- src/CoreBundle/Controller/GradeController.php
+++ src/CoreBundle/Controller/GradeController.php
@@ -12,9 +12,11 @@ use CoreBundle\Form\GroupType;
 use CoreBundle\Form\StudentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class GradeController extends Controller
 {
+
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
@@ -22,6 +24,149 @@ class GradeController extends Controller
     {
         return $this->render('CoreBundle:Default:index.html.twig');
     }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function importAction(Request $request)
+    {
+
+        if(sizeof($request->files)) {
+
+            $directory = $this->get('kernel')->getRootDir() . '/../web/uploads/';
+
+            foreach ($request->files as $uploadedFile) {
+                $name = $uploadedFile->getClientOriginalName();
+                $file = $uploadedFile->move($directory, $name);
+            }
+
+            try {
+                $inputFileType = \PHPExcel_IOFactory::identify($file);
+                $objReader = \PHPExcel_IOFactory::createReader($inputFileType);
+                $objPHPExcel = $objReader->load($file);
+            } catch(\Exception $e) {
+                die('Error loading file "'.pathinfo($file,PATHINFO_BASENAME).'": '.$e->getMessage());
+            }
+
+            $sheetData = [];
+
+            $objPHPExcel->setActiveSheetIndex(0);
+
+            $sheetData['projectClass'] = $objPHPExcel->getActiveSheet()->getCell('B5')->getValue();
+            $sheetData['advisor'] = $objPHPExcel->getActiveSheet()->getCell('D7')->getValue();
+            $sheetData['topic'] = $objPHPExcel->getActiveSheet()->getCell('D5')->getValue();
+            $sheetData['groupName'] = $sheetData['projectClass'] . ' ' . $sheetData['topic'];
+
+            $objPHPExcel->setActiveSheetIndex(13);
+
+            $sheetData['documentation'] = self::getGradeFromPointValue($objPHPExcel->getActiveSheet()->getCell('B' . 8)->getOldCalculatedValue());
+            $sheetData['product'] = self::getGradeFromPointValue($objPHPExcel->getActiveSheet()->getCell('C' . 8)->getFormattedValue());
+
+            for($i = 8; $i <= 13; $i++) {
+
+                $studentName = $objPHPExcel->getActiveSheet()->getCell('A' . $i)->getFormattedValue();
+
+                if($studentName) {
+
+                    $student = [];
+                    $student['name'] = $studentName;
+
+                    $student['presentation'] = self::getGradeFromPointValue($objPHPExcel->getActiveSheet()->getCell('F' . $i)->getOldCalculatedValue());
+                    $student['discussion'] = self::getGradeFromPointValue($objPHPExcel->getActiveSheet()->getCell('G' . $i)->getOldCalculatedValue());
+
+                    $student['total_ihk'] = self::getGradeFromPointValue($objPHPExcel->getActiveSheet()->getCell('I' . $i)->getOldCalculatedValue());
+                    $student['total_gso'] = self::getGradeFromPointValue($objPHPExcel->getActiveSheet()->getCell('J' . $i)->getOldCalculatedValue());
+
+                    $sheetData['students'][] = $student;
+                }
+            }
+        }
+
+        return $this->render('CoreBundle:Grade:import.html.twig');
+    }
+
+
+    private function getGradeFromPointValue($points) {
+
+        $a = false;
+
+        switch($points) {
+            case ($points< 10):
+                $a = "6-";
+                break;
+
+            case ($points< 25):
+                $a = "6";
+                break;
+
+            case ($points< 30):
+                $a = "6+";
+                break;
+
+            case ($points< 36):
+                $a = "5-";
+                break;
+
+            case ($points< 43):
+                $a = "5";
+                break;
+
+            case ($points< 50):
+                $a = "5+";
+                break;
+
+            case ($points< 56):
+                $a = "4-";
+                break;
+
+            case ($points< 62):
+                $a = "4";
+                break;
+
+            case ($points< 67):
+                $a = "4+";
+                break;
+
+            case ($points< 72):
+                $a = "3-";
+                break;
+
+            case ($points< 77):
+                $a = "3";
+                break;
+
+            case ($points< 81):
+                $a = "3+";
+                break;
+
+            case ($points< 85):
+                $a = "2-";
+                break;
+
+            case ($points< 89):
+                $a = "2";
+                break;
+
+            case ($points< 92):
+                $a = "2+";
+                break;
+
+            case ($points< 95):
+                $a = "1-";
+                break;
+
+            case ($points< 96):
+                $a = "1";
+                break;
+
+            case ($points<= 100):
+                $a = "1+";
+                break;
+        }
+
+        return $a;
+    }
+
 
     /**
      * @param Request $request
