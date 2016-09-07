4d4760b91cf362243da41598f7763da359cc3679
Annika Windel <annika.windel@gmail.com>
2016-09-07 16:22:01 +0200

    grade edit

--- src/CoreBundle/Controller/GradeController.php
+++ src/CoreBundle/Controller/GradeController.php
@@ -121,6 +121,111 @@ class GradeController extends Controller
 
     /**
      * @param Request $request
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction(Request $request, $id)
+    {
+        $session = $request->getSession();
+
+        $student = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('CoreBundle:Student')
+            ->findOneBy(array('id' => $id));
+
+        $form = $this->createForm(StudentType::class, $student);
+        $groupForm = $this->createForm(GroupType::class, $student->getProjectGroup());
+        $groupSelectForm = $this->createForm(GroupSelectType::class, $student->getProjectGroup());
+
+        //setting data manually for unmapped form fields
+        $projectGroup = $student->getProjectGroup();
+        $singleRating = $student->getSingleRating();
+        $form->get('discussion')->setData($singleRating->getDiscussion());
+        $form->get('presentation')->setData($singleRating->getPresentation());
+        $form->get('totalGso')->setData($singleRating->getTotalGso());
+        $form->get('totalIhk')->setData($singleRating->getTotalIhk());
+        $groupForm->get('product')->setData($projectGroup->getGroupRating()->getProduct());
+        $groupForm->get('documentation')->setData($projectGroup->getGroupRating()->getDocumentation());
+        $groupSelectForm->get('selectedGroup')->setData($student->getProjectGroup());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $studentData = $request->request->get('student');
+
+            $em->remove($singleRating);
+            $em->flush();
+            $singleRating = new SingleRating();
+            $singleRating->setStudent($student);
+            $singleRating->setDiscussion($studentData['discussion']);
+            $singleRating->setPresentation($studentData['presentation']);
+            $singleRating->setTotalGso($studentData['totalGso']);
+            $singleRating->setTotalIhk($studentData['totalIhk']);
+
+
+            if (!$request->request->get('student')['group_exists']) {
+                // group alerady exists
+
+                $selectedGroupId = $request->request->get('group_select')['selectedGroup'];
+                $selectedGroup = $this->getDoctrine()->getRepository('CoreBundle:ProjectGroup')->find($selectedGroupId);
+
+                $student->setProjectGroup($selectedGroup);
+            } else {
+                // new group
+
+                $group = new ProjectGroup();
+                $groupData = $request->request->get('group');
+                $group->setName($groupData['name']);
+
+                $advisor = $this->getDoctrine()->getRepository('CoreBundle:Advisor')->find($groupData['advisor']);
+                $topic = $this->getDoctrine()->getRepository('CoreBundle:Topic')->find($groupData['topic']);
+                $projectClass = $this->getDoctrine()->getRepository('CoreBundle:ProjectClass')->find($groupData['projectClass']);
+
+                $group->setAdvisor($advisor);
+                $group->setTopic($topic);
+                $group->setProjectClass($projectClass);
+
+                $groupRating = new GroupRating();
+                $groupRating->setDocumentation($groupData['documentation']);
+                $groupRating->setProduct($groupData['product']);
+                $groupRating->setProjectGroup($group);
+                $group->setGroupRating($groupRating);
+
+                $student->setProjectGroup($group);
+
+                $em->persist($group);
+            }
+
+            $student->setSingleRating($singleRating);
+
+            $em->persist($student);
+            $em->flush();
+
+            $session->getFlashBag()->add(
+                'success',
+                'Die Änderungen wurden erfolgreich gespeichert!'
+            );
+
+            return $this->redirect($this->generateUrl(
+                'student_detail', array('id' => $student->getId())
+            ));
+
+        }
+
+        return $this->render(
+            'CoreBundle:Grade:edit.html.twig',
+            array(
+                'form' => $form->createView(),
+                'groupForm' => $groupForm->createView(),
+                'groupSelectForm' => $groupSelectForm->createView(),
+            )
+        );
+    }
+
+    /**
+     * @param Request $request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function searchAction(Request $request)
